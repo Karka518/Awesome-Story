27e211949eb6a98dfe6ae2acfe2840e1de7aabbc
Karka518 <karsen.box@gmail.com>
2015-11-08 07:55:11 -0700

    error fixed
    I looked at the code from earlier and found my problem

--- script.js
+++ script.js
@@ -29,9 +29,8 @@ options.addEventListener('click', function  (e) {
 	
 	} else if (e.target.innerHTML === 'ignore') {
 		text.innerHTML = 'ignore this'
-		story.ok.options.forEach(function (item) {
-			options.innerHTML += '<li>' + item + '</li>'
-	}) else {
+
+	} else {
 		text.innerHTML = story.start.text
 		options.innerHTML = ''
 		story.start.options.forEach(function (item) {
